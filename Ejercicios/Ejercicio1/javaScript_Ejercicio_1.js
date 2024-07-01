fc2ddcfedc354c686301d02ce3261687c7e001f3
AilynEncinas <168148535+AilynEncinas@users.noreply.github.com>
2024-07-01 10:33:27 -0400

    Add files via upload
    Este es el Ejercicio 1 y su concepto se encuentra en las siguientes Pags 7-8

---
+++ Ejercicios/Ejercicio1/javaScript_Ejercicio_1.js
@@ -0,0 +1,4 @@
+function cambiarColor() {
+    document.body.style.backgroundColor="#0000FF";//Llamada al cuerpo del documento html para que cambie de color
+}
+document.addEventListener("click",cambiarColor);//accion para que cuando se haga un click se pueda cambiar el color
